c48c94c796955f70065a79ef159bccbf8f8db53a
sunnythakur151994-sudo <sunnythakur151994@gmail.com>
2026-05-10 19:11:17 +0530

    Add functionality to publish a blog post

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print("Bot Started")
+from blogger_post import publish_post
+
+title = "My First AI Blog"
+
+content = """
+<h1>Hello World</h1>
+
+<p>This blog was automatically posted using GitHub Actions and Python.</p>
+"""
+
+publish_post(title, content)
